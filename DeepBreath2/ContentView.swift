44bd369555d1fd96bd2ca098223a5bd45bd1c2f2
Paavan Buddhdev <paavan.buddhdev@gmail.com>
2024-08-26 10:10:10 +0100

    Initial Commit

---
+++ DeepBreath2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepBreath2
+//
+//  Created by Paavan Buddhdev on 26/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepBreath2/DeepBreath2App.swift
@@ -0,0 +1,17 @@
+//
+//  DeepBreath2App.swift
+//  DeepBreath2
+//
+//  Created by Paavan Buddhdev on 26/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DeepBreath2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
